186040c386ac8200d40989607ced3907e11c7ccd
Rodrigo <rodrigo.rm123.rm@gmail.com>
2024-01-04 14:38:34 -0300

    feat: add method index and create at ProductController

--- api/src/app/controllers/ProductController.ts
+++ api/src/app/controllers/ProductController.ts
@@ -1,15 +1,56 @@
 import { Request, Response } from 'express';
+import ProductRepository from '../repositories/ProductRepository';
 
 class ProductController {
-  index(request: Request, response: Response) {
-    return response.json({message: 'acesso a rota /products'});
+  async index(request: Request, response: Response) {
+    try {
+      const products = await ProductRepository.findAll();
+
+      return response.json(products);
+    } catch (error) {
+      throw new Error('Error to list products');
+    }
   }
-  store(request: Request, response: Response) {
-    return response.json({message: 'acesso a rota criar /products'});
+
+  async store(request: Request, response: Response) {
+    try {
+      const {
+        name,
+        description,
+        price,
+        brand,
+        weightInGrams,
+        quantity,
+        imagePath,
+        category,
+        inPromotion,
+        ingredients
+      } = request.body;
+
+      const product = await ProductRepository.create({
+        name,
+        price,
+        description,
+        brand,
+        weightInGrams,
+        quantity,
+        imagePath,
+        category,
+        inPromotion,
+        ingredients
+      });
+
+      response.json(product);
+
+    } catch (error) {
+      throw new Error('Error to create product');
+    }
   }
+
   update(request: Request, response: Response) {
     return response.json({message: 'acesso rota atualizar /products'});
   }
+
   delete(request: Request, response: Response) {
     return response.json({message: 'acesso rota deletar /products'});
   }
