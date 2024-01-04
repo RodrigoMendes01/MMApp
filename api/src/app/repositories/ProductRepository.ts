8430322203addc3a4b0c139a7f15e2e8bbb3aa29
Rodrigo <rodrigo.rm123.rm@gmail.com>
2024-01-04 14:38:02 -0300

    feat: add ProductRepository

---
+++ api/src/app/repositories/ProductRepository.ts
@@ -0,0 +1,55 @@
+import { Product } from '../models/Product';
+
+interface ProductValuesTypes {
+  id?: object,
+  name: string,
+  description: string,
+  price: number,
+  brand: string,
+  quantity: number,
+  weightInGrams: number
+  imagePath: string,
+  category: string,
+  inPromotion: boolean
+  ingredients: {
+  calories: string,
+  carbohydrates: string,
+  protein: string,
+  totalFat: string,
+  sodium: string,
+  }
+}
+
+
+class ProductRepository {
+  async findAll() {
+    const documents = await Product.find({});
+
+    return documents;
+  }
+
+  async create(data: ProductValuesTypes) {
+    const document = await Product.create({
+      name: data.name,
+      description: data.description,
+      price: data.price,
+      brand: data.brand,
+      quantity: data.quantity,
+      weightInGrams: data.weightInGrams,
+      imagePath: data.imagePath,
+      category: data.category,
+      inPromotion: data.inPromotion,
+      ingredients: {
+        calories: data.ingredients.calories,
+        carbohydrates: data.ingredients.carbohydrates,
+        protein: data.ingredients.protein,
+        totalFat: data.ingredients.totalFat,
+        sodium: data.ingredients.sodium
+      }
+    });
+
+    return document;
+  }
+}
+
+export default new ProductRepository();
