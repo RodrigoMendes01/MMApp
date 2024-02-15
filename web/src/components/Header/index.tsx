5cf23b856b7cfc45a99dce7651c401ae87dc381e
Rodrigo <rodrigo.rm123.rm@gmail.com>
2024-02-14 22:11:00 -0300

    feat: add style to header

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,22 +1,24 @@
 import { Container } from "./styles"
+import brand from '../../assets/svg/carrinho.png';
+import lupa from '../../assets/svg/lupa.svg';
+import user from '../../assets/svg/user-icon.svg'
+import bag from '../../assets/svg/saco-de-papel.svg'
 
 export default function Header () {
   return (
     <Container>
-      <div id="company_brand">
-        LOGO
+      <div id="company-brand">
+        <img src={brand} alt="Carrinho de compras"/>
       </div>
 
       <div id="search-bar">
-        <div>
-          <input type="text" />
-          <img src="" alt="" />
-        </div>
+          <input type="text" placeholder="Pesquisa..."/>
+          <img src={lupa} alt="Lupa de Pesquisa" />
       </div>
 
       <div id="profile-car">
-        <div>USER</div>
-        <div>CARRINHO</div>
+        <div><a href="/login"><img src={bag} alt="" /></a></div>
+        <div><a href="/products"><img src={user} alt=""/></a></div>
       </div>
     </Container>
   )
--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -4,5 +4,51 @@ export const Container = styled.header`
   background-color: ${({theme}) => theme.colors.theme};
   height: 80px;
   display: flex;
+  padding: 5px;
   justify-content: space-around;
+
+  #company-brand {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      height: 65px;
+      transform: scaleX(-1);
+    }
+  }
+
+  #search-bar {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 600px;
+    gap: 5px;
+
+    input {
+      flex-grow: 1;
+      height: 40px;
+      border: none;
+      border-radius: 6px;
+      padding-left: 6px;
+      outline: none;
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    }
+
+    img {
+      width: 35px;
+    }
+  }
+
+  #profile-car {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+
+    img {
+      height: 35px;
+    }
+  }
 `
